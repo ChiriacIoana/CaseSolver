28f2bebfbfff65432fb2d38bf287cb81294fda33
ChiriacIoana <chiriacioana2008@gmail.com>
2026-01-07 19:53:41 +0200

    idk

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/',
-        destination: '/welcome',
+        destination: '/register',
         permanent: true, 
       },
     ]
